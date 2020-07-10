a8ffff77b5270f0bb8ee229b9149eae21883ed51
EricBollar <esbollar@gmail.com>
2020-07-09 20:20:44 -0700

    completed left and up functions

--- Homework1/hw1-linux_osx/Transform.cpp
+++ Homework1/hw1-linux_osx/Transform.cpp
@@ -1,14 +1,14 @@
 // Transform.cpp: implementation of the Transform class.
 
-
 #include "Transform.h"
+#include <math.h>
 
 //Please implement the following functions:
 
 // Helper rotation function.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) {
 	// YOUR CODE FOR HW1 HERE
-	
+
 	// You will change this return call
 	return mat3();
 }
@@ -16,11 +16,44 @@ mat3 Transform::rotate(const float degrees, const vec3& axis) {
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
 	// YOUR CODE FOR HW1 HERE
+	vec3 axis = glm::normalize(up);
+	mat3 aat = mat3(
+		axis[0]*axis[0], axis[0]*axis[1], axis[0]*axis[2],
+		axis[1]*axis[0], axis[1]*axis[1], axis[1]*axis[2],
+		axis[2]*axis[0], axis[2]*axis[1], axis[2]*axis[2]
+	);
+	mat3 astar = mat3(
+		0, -axis[2], axis[1],
+		axis[2], 0, -axis[0],
+		-axis[1], axis[0], 0
+	);
+	mat3 identity = mat3(1);
+	// Rodrigues' Rotation Formula
+	mat3 rotMat = cosf(degrees)*identity + (1-cosf(degrees))*aat + sinf(degrees)*astar;
+	eye = rotMat * eye;
+	up = rotMat * up;
 }
 
 // Transforms the camera up around the "crystal ball" interface
 void Transform::up(float degrees, vec3& eye, vec3& up) {
 	// YOUR CODE FOR HW1 HERE 
+	vec3 forward = glm::normalize(vec3(0, 0, 0) - eye);
+	vec3 axis = glm::cross(glm::normalize(up), forward);
+	mat3 aat = mat3(
+		axis[0]*axis[0], axis[0]*axis[1], axis[0]*axis[2],
+		axis[1]*axis[0], axis[1]*axis[1], axis[1]*axis[2],
+		axis[2]*axis[0], axis[2]*axis[1], axis[2]*axis[2]
+	);
+	mat3 astar = mat3(
+		0, -axis[2], axis[1],
+		axis[2], 0, -axis[0],
+		-axis[1], axis[0], 0
+	);
+	mat3 identity = mat3(1);
+	// Rodrigues' Rotation Formula
+	mat3 rotMat = cosf(degrees)*identity + (1-cosf(degrees))*aat + sinf(degrees)*astar;
+	eye = rotMat * eye;
+	up = rotMat * up;
 }
 
 // Your implementation of the glm::lookAt matrix
